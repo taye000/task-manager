ce7ddda7bb19865fd46a807bcc6e0d2bce3bda27
taye000 <taylorgitari@gmail.com>
2023-01-06 14:32:00 +0300

    on toggle

--- src/App.js
+++ src/App.js
@@ -23,10 +23,23 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  //toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder:!task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "It's lonely in here, add some tasks."
+      )}
     </div>
   );
 }
