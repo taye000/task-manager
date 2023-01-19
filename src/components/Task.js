6ce2bdddeccfed13b66ba08a6252ceec77ba0047
taye000 <taylorgitari@gmail.com>
2023-01-19 12:00:01 +0300

    task1 css

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,14 +6,20 @@ const Task = ({ task, onDelete, onToggle }) => {
       className={`task ${task.reminder && "reminder"}`}
       onDoubleClick={() => onToggle(task.id)}
     >
-      <h3>
-        {task.text}{" "}
-        <FaTimes
-          style={{ color: "red", cursor: "pointer" }}
-          onClick={() => onDelete(task.id)}
-        />
-      </h3>
-      <p>{task.day}</p>
+      <div className="task1">
+        <p>{task.day}</p>
+        <h3>
+          {task.text}{" "}
+          <FaTimes
+            style={{
+              justifyContent: "space-between",
+              color: "red",
+              cursor: "pointer",
+            }}
+            onClick={() => onDelete(task.id)}
+          />
+        </h3>
+      </div>
     </div>
   );
 };
